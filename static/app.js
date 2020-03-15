d5882c4bdf77cd612a1c02d8182c77c4f2a54e84
Semyon <serdtsev.sd@gmail.com>
2020-03-15 20:49:42 +0300

    master

--- static/app.js
+++ static/app.js
@@ -42,32 +42,18 @@ var convApp = new Vue({
     }
 })
 
+
 var login = new Vue({
-    el: '#login',
+    el: '#loginApp',
     data: {
         usernameLogin: null,
     },
     methods: {
-        createdUsernameLogin: function () {
-            console.log('username', this.usernameLogin);
-        },
-    },
 
-})
-
-var signup = new Vue({
-    el: '#signup',
-    data: {
-        usernameSignup: null,
-    },
-    methods: {
-
-        createdUsernameSignup: function () {
-            console.log('username', this.usernameSignup);
-        },
-        setUser: function () {
-            if (this.usernameSignup) {
-                this.$http.post('http://localhost:9090/', { username: this.usernameSignup }).then(response => {
+        login: function () {
+            if (this.usernameLogin) {
+                this.$http.post('http://localhost:9090/', { username: this.usernameLogin }).then(response => {
+                    console.log(response);
                     ;
                 }, response => {
                     console.log("error:", response)
